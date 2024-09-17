a22d9f173176b387027fc49d5db53dfc9d7ad7b2
yoz75 <levtannn@gmail.com>
2024-09-17 19:16:09 +0300

    Added powder fliudness

--- Assets/Sources/Simulation/Cells/Powder.cs
+++ Assets/Sources/Simulation/Cells/Powder.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Powder : Cell
 {
+
+    protected float Fluidness = 0.05f;
+
     public override void Init()
     {
         Color = Color.blue;
@@ -13,6 +16,32 @@ public class Powder : Cell
         if(neighbors.Down is VoidCell)
         {
             SwapPositions(this, neighbors.Down);
+            return;
+        }
+
+        System.Random random = new System.Random();
+        if(neighbors.Down is Powder && neighbors.DownLeft is VoidCell)
+        {
+            var value = random.NextDouble();
+
+            if(value >= Fluidness)
+            {
+                SwapPositions(this, neighbors.DownLeft);
+            }
+        }
+        else if(neighbors.Down is Powder && neighbors.DownRight is VoidCell)
+        {
+            var value = random.Next(0, 2);
+
+            if(value == 0)
+            {
+                return;
+            }
+
+            if(value == 1)
+            {
+                SwapPositions(this, neighbors.DownRight);
+            }
         }
     }
 }
